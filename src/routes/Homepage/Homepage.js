43c73095fd9f0d077b6ed40b03b7a2ae1a76635b
mismyl123 <mlalonde309@gmail.com>
2020-12-30 01:58:58 -0500

    fixed error

--- src/routes/Homepage/Homepage.js
+++ src/routes/Homepage/Homepage.js
@@ -6,11 +6,11 @@ export default class Hompage extends Component {
     return (
       <div className="Homepage">
         <div className="page-container">
-          <h1 className="welcome">Welcome to Petful Adoption Service</h1>
+          <h1 className="welcome">Petful FIFO Adoption Center</h1>
           <div className="process-container">
-            <h2>How the Adoption Process Works:</h2>
+            <h2>Thank You For Visiting Our Center!</h2>
             <p>
-            We operate on a First in First out basis.
+            How we work: We operate on a First in First out basis.
             We believe all pets should be loved and treasured equally.
             Cat person or dog person we don't care, our only rule is 
             that you wait your turn and adopt the next available pet of 
@@ -23,7 +23,7 @@ export default class Hompage extends Component {
               <Link className="adopt" to="/adoption">
                 Adopt
               </Link>{' '}
-              to begin the process!
+              to meet your new best friend!
             </h3>
           </div>
         </div>
