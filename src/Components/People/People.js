2901c86349501970604baaa51ccbe7e9c6d5ca8b
mismyl123 <mlalonde309@gmail.com>
2020-12-31 11:21:21 -0500

    required changes

--- src/Components/People/People.js
+++ src/Components/People/People.js
@@ -9,6 +9,10 @@ export default class People extends Component {
     return (
       <div className="people-container">
         <h1>Add Your Name to the Adoption Queue:</h1>
+        <p>Submit your name in the box below.</p>
+        <p>You will be added to the end of the line.</p>
+        <p>When you reach the top of the list, you will be </p>
+        <p>shown the next available dog or cat to adopt!</p>
         <ul>{this.renderLine()} </ul>
         {!this.props.inLine && (
           <AddForm
